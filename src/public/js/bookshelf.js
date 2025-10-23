e14f1e6fdc071ab22ca19e40c44468f1705d5a66
Dane Patzlaff <patzlaffdane@gmail.com>
2025-10-23 16:38:38 -0500

    Added dropdown button, still needs work

--- src/public/js/bookshelf.js
+++ src/public/js/bookshelf.js
@@ -2,11 +2,22 @@
  * 
  * 
  */
+function toggleDropdown() {
+    document.getElementById("books-dropdown-menu").classList.toggle("show");
+  }
 
+  // Optional: close dropdown if user clicks outside
+  window.onclick = function(event) {
+    if (!event.target.matches('.managebooks')) {
+      const dropdowns = document.getElementsByClassName("books-dropdown-content");
+      for (let i = 0; i < dropdowns.length; i++) {
+        dropdowns[i].classList.remove('show');
+      }
+    }
+  }
 
 //Create code for adding, deleting, updating/changing, and moving a book
-//Takes a String with the add, del, upd, mov 
-
+//Takes a String with the add, del, upd, mov
 
 /**
  * Create the book constructor
@@ -15,31 +26,9 @@
  * 
  */
 
-
 /***
  * 
  * document.querySelector("#author").value = 
  * 
  * 
  */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
